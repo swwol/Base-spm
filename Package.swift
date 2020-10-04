3cc79e480536d834cc401da177d9a480a8d3b593
sam woolf <sam.woolf@gmail.com>
2020-10-05 00:00:52 +0100

    update minimum ios version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Base",
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,8 +14,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ashleymills/Reachability.swift", .exact("4.3.1")),
-        .package(url: "https://github.com/swwol/Log-spm/", from: "1.0.0")
+        .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift", .exact("4.3.1")),
+        .package(name: "Log", url: "https://github.com/swwol/Log-spm/", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
